e7036afe8308b7ca5a24f0bdc3d255373b6d731e
fzoozai <fabian.soosaithasan@gmail.com>
2019-10-19 11:58:17 +0200

    Write get endpoint for specific entry filtered by id

--- src/main/java/io/fzoozai/springboot/topic/TopicController.java
+++ src/main/java/io/fzoozai/springboot/topic/TopicController.java
@@ -3,12 +3,12 @@ package io.fzoozai.springboot.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
 	
 	@Autowired
 	private TopicService topicService;
@@ -19,5 +19,9 @@ public class TopicController {
 		return topicService.getAllTopics();
 	}
 	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
 	
 }
